417a270c4c6b051e73fb1cc6c56935732fe48b36
Alexander Dikusar <alexander.dikusar@additiv.ch>
2019-11-28 02:04:38 +0300

    fix for task-1

--- task-1/index.js
+++ task-1/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 7780;
 
+function createForm(loginValue = '') {
+  let formTemplate = fs.readFileSync('task-1/form.html'),
+      loginTemplate = '{login}';
+  return formTemplate.toString().replace(loginTemplate, loginValue);
+}
+
 app.get('/', function (req, res) {
-  res.sendfile('./form.html');
+  res.send(createForm());
 });
 
 app.get('/login', function (req, res) {
@@ -13,7 +20,7 @@ app.get('/login', function (req, res) {
   if (login && password) {
     response = `Hi ${login}! You have logged in successfully!`;
   } else {
-    response = 'Sorry, something went wrong...';
+    response = 'Sorry, something went wrong... Please try again' + createForm(login);
   }
 
   res.send(response);
